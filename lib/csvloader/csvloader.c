b4bf9fa32ed2bfc864e859535145f77d9fc8b4f8
loxygenK <loxygen.k@gmail.com>
2020-10-09 11:18:36 +0900

    Added the load method's denifition

---
+++ lib/csvloader/csvloader.c
@@ -0,0 +1,10 @@
+#include "csvloader.h"
+#include <stdio.h>
+
+User *loaduser(FILE *fp) {
+	printf("Hello from loaduser");
+}
+
+int saveuser(FILE *fp, User *users) {
+	printf("Hello from saveuesr\n");
+}
--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+#include "lib/csvloader/csvloader.h"
+
 int main() {
 
-	printf("Hello world!\n");
+	FILE *fp = fopen("list.csv", "r");
+	loaduser(fp);
+	fclose(fp);
 	return 0;
 }
