c93910864c826b96396efbe1b0e615336df5ea45
loxygenK <loxygen.k@gmail.com>
2020-10-09 14:56:58 +0900

    Added comment

--- lib/csvloader/csvloader.c
+++ lib/csvloader/csvloader.c
@@ -52,6 +52,7 @@ int saveuser(const char* path, const UserList* user) {
   // If laod was failure, put error message and return
   if(fp == NULL) return -1;
 
+  // Print informations to csv
   for(int i = 0;i<user->number;i++) {
     fprintf(
       fp, "%d,%s,%s,%s\n",
@@ -60,6 +61,7 @@ int saveuser(const char* path, const UserList* user) {
     );
   }
 
+  // close file stream
   fclose(fp);
 
   return 0;
@@ -69,23 +71,34 @@ int adduser(
     UserList *user,
     int number, const char *name, const char *ruby, const char *school
 ) {
+  // Check overflow
   if((user->number + 1) >= MAX_CSV_SIZE) return 1;
+
+  // Set new user
   user->users[user->number].number = number;
   strcpy(user->users[user->number].name, name);
   strcpy(user->users[user->number].ruby, ruby);
   strcpy(user->users[user->number].school, school);
+
+  // Add counter
   user->number++;
+
   return 0;
 }
 
 int removeuser(UserList *user, int index) {
+  // Check the index is valid
   if(index < 0 || index >= user->number) return 1;
+
+  // Shift user informations
   for(int i = index; i<user->number-1; i++) {
     user->users[i].number = user->users[i+1].number;
     strcpy(user->users[i].name, user->users[i+1].name);
     strcpy(user->users[i].ruby, user->users[i+1].ruby);
     strcpy(user->users[i].school, user->users[i+1].school);
   }
+
+  // Decrement counter
   user->number--;
   return 0;
 }
