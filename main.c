dcf9419c447b0818a5b5bbac1a04a7f1d808157b
loxygenK <loxygen.k@gmail.com>
2020-10-09 14:22:15 +0900

    Divided operations into several functions

--- main.c
+++ main.c
@@ -2,20 +2,29 @@
 
 #include "lib/csvloader/csvloader.h"
 
-int main() {
-  User *user;
+int loadfromcsv(User *dest) {
   int result;
-  result = loaduser("list.csv", user);
+  result = loaduser("list.csv", dest);
   if(result < 0) {
     printf("Loaduser function returned an negative value (means failed to load file)!\n");
-    return 1;
+    return 0;
   }
   printf("Loaduser has loaded %d user(s).\n", result);
+  return result;
+}
+
+void load(){
+  User user[MAX_CSV_SIZE];
+  int result = loadfromcsv(user);
   for(int i = 0;i<result;i++){
     printf(
         "%2d: #%2d, %s[%s] (from %s)\n", i,
         user[i].number, user[i].name, user[i].ruby, user[i].school
     );
   }
+}
+
+int main() {
+  load();
   return 0;
 }
