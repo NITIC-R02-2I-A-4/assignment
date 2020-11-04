29b646e0827f292c7c211dc175262ac95d9add58
kyosuke-okano <st19047ky@gm.ibaraki-ct.ac.jp>
2020-11-04 14:01:45 +0900

    test2'

--- lib/print_by_name.c
+++ lib/print_by_name.c
@@ -1,15 +1,7 @@
 #include <string.h>
-int print_by_name(int number char* query_name,  char name[]) {
-int noa;
-int student_number;
-for(noa = 0; noa < number ; noa++){
-if(strcmp (query_name ,name[noa])== 0){
-student_number = noa + 1;
-}
-}
-printf("%d",student_number);
-return 0;
-}int print_by_name(int number char* query_name,  char name[]) {
+#include<stdio.h>
+#include"macros.h"
+int print_by_name(int number, char* query_name,  char name[][MAX_STR_SIZE]) {
 int noa;
 int student_number;
 for(noa = 0; noa < number ; noa++){
